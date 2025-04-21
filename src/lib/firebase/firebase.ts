e639cf9d87f95ed341931f014e007eece4e4ea83
Aryan Sanghi <aryans-san-008@gmail.com>
2025-04-22 00:47:39 +0530

    firebase key config

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAuYRqAN6yqrujwlKXLfrQGGgxRPo_S45E",
-  authDomain: "stickify-aryan-sanghi.firebaseapp.com",
-  projectId: "stickify-aryan-sanghi",
-  storageBucket: "stickify-aryan-sanghi.appspot.com",
-  messagingSenderId: "601297238583",
-  appId: "1:601297238583:web:6d9448012a747bf5cdddb2",
-  measurementId: "G-9Z8MVC17PB",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
