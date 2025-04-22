c05bb40fb79dae978ad9d469a5a13914e54aef82
Aryan Sanghi <aryans-san-008@gmail.com>
2025-04-22 12:53:31 +0530

    render config updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import fs from 'fs';
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: '/notify/',
+  plugins: [
+    tailwindcss(),
+    react(),
+    {
+      name: "copy-index-to-404",
+      closeBundle: () => {
+        const indexPath = "./dist/index.html";
+        const errorPath = "./dist/404.html";
+        if (fs.existsSync(indexPath)) {
+          fs.copyFileSync(indexPath, errorPath);
+        }
+      },
+    },
+  ],
+  base: "/notify/",
 });
